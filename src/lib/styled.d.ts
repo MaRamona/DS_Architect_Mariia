6fd5498eeabb8858a14ef186fcca2f6a03cffab5
Marmarella <yomydesign@gmail.com>
2025-08-12 01:35:37 +0300

    bg tapization

--- src/lib/styled.d.ts
+++ src/lib/styled.d.ts
@@ -3,8 +3,70 @@ import "styled-components";
 declare module "styled-components" {
     export interface DefaultTheme {
         colors: {
+            bgGlobal: {
+                levelOne: string;
+                levelTwo: string;
+                levelThree: string;
+            };
+
+            bgInteractive: {
+                error: {
+                    primary: {
+                        default: string;
+                        hover: string;
+                        click: string;
+                        disabled: string;
+                    };
+                    secondary: {
+                        default: string;
+                        hover: string;
+                        click: string;
+                        disabled: string;
+                    };
+                };
+                
+                brand: {
+                    primary: {
+                        default: string;
+                        hover: string;
+                        click: string;
+                        disabled: string;
+                    };
+                    secondary: {
+                        default: string;
+                        hover: string;
+                        click: string;
+                        disabled: string;
+                    };
+                };
+
+                success: {
+                    primary: {
+                        default: string;
+                        hover: string;
+                        click: string;
+                        disabled: string;
+                    };
+                    secondary: {
+                        default: string;
+                        hover: string;
+                        click: string;
+                        disabled: string;
+                    };
+                };
+
+            bgStatic: {
+                error: {
+                    primary: string;
+                    secondary: string;
+                    disabled: string;
+                };
+            };
+
+        
+            };
             text: {
-            main: string;
+            primary: string;
             }
         };
     };
